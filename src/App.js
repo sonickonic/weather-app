ad33bc743633c5e0cdca714337db78e89d01aacc
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-07-28 13:56:54 +0100

    Integrate Real-Time Weather API
    - At the moment the query is fixed for development purposes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [weather, setWeather] = useState();
+  const baseEndpoint = "http://api.weatherstack.com/current";
+
+  async function fetchWeather(query) {
+    const accessKey = "2efb97218011e382b9523f8267818ed5";
+    const res = await fetch(
+      `${baseEndpoint}?access_key=${accessKey}&query=${query}`
+    );
+    const data = await res.json();
+    setWeather(data);
+  }
+
+  useEffect(() => {
+    fetchWeather("london");
+  }, []);
+
   return <div></div>;
 }
 

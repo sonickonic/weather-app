551635c2477020883c8ca932ce2bdce5624a48df
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-08-03 13:24:19 +0100

    Updates

---
+++ precache-manifest.6f24ff3a9990449942ee91d731c254c8.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95d11a9ea85d89c6b7e138bbd7af799",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "b62bdd1c00ba0ee54df9",
+    "url": "/weather-api/static/css/main.2a11c181.chunk.css"
+  },
+  {
+    "revision": "7e31d53663e794cd0fc7",
+    "url": "/weather-api/static/js/2.8efb5574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-api/static/js/2.8efb5574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62bdd1c00ba0ee54df9",
+    "url": "/weather-api/static/js/main.6b3edae8.chunk.js"
+  },
+  {
+    "revision": "13585e8ad9ec8fc06ead",
+    "url": "/weather-api/static/js/runtime-main.dc44b75c.js"
+  },
+  {
+    "revision": "085d2a3a78add4d59b62d13740f7a4f3",
+    "url": "/weather-api/static/media/113.085d2a3a.svg"
+  },
+  {
+    "revision": "8e03bb3f6206c22b27083bd360c92812",
+    "url": "/weather-api/static/media/116.8e03bb3f.svg"
+  },
+  {
+    "revision": "085d2a3a78add4d59b62d13740f7a4f3",
+    "url": "/weather-api/static/media/119.085d2a3a.svg"
+  },
+  {
+    "revision": "9304cc1b3eaee47a72c82260ef76540c",
+    "url": "/weather-api/static/media/122.9304cc1b.svg"
+  },
+  {
+    "revision": "0ee1dfb3800a1de5bd01b85756d4e10a",
+    "url": "/weather-api/static/media/143.0ee1dfb3.svg"
+  },
+  {
+    "revision": "1ed752c0d140db13b1a9afb7a2af26ee",
+    "url": "/weather-api/static/media/176.1ed752c0.svg"
+  },
+  {
+    "revision": "099530bdb34bdb2c56c2bfa6c72df3b4",
+    "url": "/weather-api/static/media/179.099530bd.svg"
+  },
+  {
+    "revision": "4298ebd7ab09b2a6065841bf2770496a",
+    "url": "/weather-api/static/media/182.4298ebd7.svg"
+  },
+  {
+    "revision": "a7ab1024418092e980951a720a9282cd",
+    "url": "/weather-api/static/media/185.a7ab1024.svg"
+  },
+  {
+    "revision": "3b322e63892d52d951220fa251a9ccfd",
+    "url": "/weather-api/static/media/200.3b322e63.svg"
+  },
+  {
+    "revision": "5f790d8da89c416792d55404886cc976",
+    "url": "/weather-api/static/media/227.5f790d8d.svg"
+  },
+  {
+    "revision": "dc9af321617eab16401f2a3d63fb1e65",
+    "url": "/weather-api/static/media/230.dc9af321.svg"
+  },
+  {
+    "revision": "a5ce8517c482337b8692df4444212612",
+    "url": "/weather-api/static/media/248.a5ce8517.svg"
+  },
+  {
+    "revision": "65b06aa4815438f45c28b1ccbb809208",
+    "url": "/weather-api/static/media/260.65b06aa4.svg"
+  },
+  {
+    "revision": "8b8907706190db8096e915e3418602c5",
+    "url": "/weather-api/static/media/263.8b890770.svg"
+  },
+  {
+    "revision": "4da61c6c07e48c311bdc2e8317d87979",
+    "url": "/weather-api/static/media/266.4da61c6c.svg"
+  },
+  {
+    "revision": "82f29e1279935e4dd4536637fd11b336",
+    "url": "/weather-api/static/media/281.82f29e12.svg"
+  },
+  {
+    "revision": "b8712e1e54fe7af1f3f6039a8eb50e91",
+    "url": "/weather-api/static/media/284.b8712e1e.svg"
+  },
+  {
+    "revision": "3f478fd99a3af57e7ce8910fe58816fc",
+    "url": "/weather-api/static/media/293.3f478fd9.svg"
+  },
+  {
+    "revision": "3f478fd99a3af57e7ce8910fe58816fc",
+    "url": "/weather-api/static/media/296.3f478fd9.svg"
+  },
+  {
+    "revision": "47877eb82551a42ca3f4a7605c51fa7d",
+    "url": "/weather-api/static/media/299.47877eb8.svg"
+  },
+  {
+    "revision": "47877eb82551a42ca3f4a7605c51fa7d",
+    "url": "/weather-api/static/media/302.47877eb8.svg"
+  },
+  {
+    "revision": "1e534f71ef0efe56b5930a99b3b81c23",
+    "url": "/weather-api/static/media/305.1e534f71.svg"
+  },
+  {
+    "revision": "1e534f71ef0efe56b5930a99b3b81c23",
+    "url": "/weather-api/static/media/308.1e534f71.svg"
+  },
+  {
+    "revision": "4a9fa2234aee51c1129b66d30854062c",
+    "url": "/weather-api/static/media/arrow.4a9fa223.svg"
+  },
+  {
+    "revision": "34c938d27c12eb5052668b9a36b2e63c",
+    "url": "/weather-api/static/media/search.34c938d2.svg"
+  }
+]);
